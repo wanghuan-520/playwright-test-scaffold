9d167155e9e32a5121f456685e6c541d526ed06f
wanghuan-520 <huan.wang@aelf.io>
2026-01-09 15:05:46 +0800

    chore: remove research-agent-ui prototype (no longer needed)

--- prototypes/research-agent-ui/components/stages/index.ts
+++
@@ -1,7 +0,0 @@
-export { InputStage } from './InputStage'
-export { BriefingStage } from './BriefingStage'
-export { RunningStage } from './RunningStage'
-export { ComputeStage } from './ComputeStage'
-export { MonitoringStage } from './MonitoringStage'
-export { DeliveryStage } from './DeliveryStage'
-
